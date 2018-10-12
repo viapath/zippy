1c7abe1cac22eea4b1353ad3966c3b4920f5026c
Lucioric2000 <lucioric2000@hotmail.com>
2018-10-12 08:19:38 +0200

    Updated installer

--- zippy_run_sample.bash
+++ zippy_run_sample.bash
@@ -2,9 +2,7 @@
 sudo yum -y upgrade
 sudo yum install less make wget curl vim
 make -f Makefile_centos genome #genome index and genome download
-#https://ftp.ncbi.nlm.nih.gov/snp/organisms/human_9606_b147_GRCh37p13/VCF/00-common_all.vcf.gz
 make -f Makefile_centos install
-#make -f Makefile_centos genome-index
 make -f Makefile_centos variation-download
 make -f Makefile_centos refgene-download
 make -f Makefile_centos webservice
