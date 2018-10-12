90af3489b2964b48879b83fc0a5a901d9ff1663e
lucioric <lucioric2000@hotmail.com>
2018-10-12 02:40:00 -0500

    Update zippy_install_CentOS7.bash
    Change installation order for the program software and the directory and users structure installed first and the data last, letting the genome download (very probably the slowest) to the end

--- zippy_install_CentOS7.bash
+++ zippy_install_CentOS7.bash
@@ -17,8 +17,8 @@ else
     cd zippy
 fi
 
-make -f Makefile_centos genome #genome index and genome download
 make -f Makefile_centos install
+make -f Makefile_centos webservice
 make -f Makefile_centos variation-download
 make -f Makefile_centos refgene-download
-make -f Makefile_centos webservice
+make -f Makefile_centos genome #genome index and genome download
