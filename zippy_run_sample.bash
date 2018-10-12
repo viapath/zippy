2bd84a3e7aae91fbe82e9dc999831aabee919af5
Lucioric2000 <lucioric2000@hotmail.com>
2018-10-12 08:29:45 +0200

    Updated installer

--- zippy_run_sample.bash
+++ zippy_run_sample.bash
@@ -1,6 +1,22 @@
 #!/bin/bash
 sudo yum -y upgrade
 sudo yum install less make wget curl vim
+srv_zippy=/srv/zippy
+
+qseqdnamatch=`expr match "$(pwd)" '.*\(zippy\)'`
+if [[ $qseqdnamatch = "zippy" ]]
+then
+    echo "Already in zippy folder."
+    sudo chmod -R 777 ${srv_zippy}
+else
+    echo "Not in zippy folder."
+    sudo mkdir -p ${srv_zippy}
+    cd /srv
+    sudo chmod -R 777 ${srv_zippy}
+    git clone --recursive https://github.com/Lucioric2000/zippy
+    cd zippy
+fi
+
 make -f Makefile_centos genome #genome index and genome download
 make -f Makefile_centos install
 make -f Makefile_centos variation-download
