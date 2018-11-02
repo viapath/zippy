d069f828f920b12dfb110e8af2c5318210b34df0
Lucio Montero <luciric2000@hotmail.com>
2018-11-02 08:42:18 +0100

    Fixed installer

--- zippy_install.bash
+++ zippy_install.bash
@@ -9,6 +9,12 @@ then
     echo "Already in zippy folder."
     sudo chmod -R 777 ${srv_zippy}
     git pull origin master
+	make cleansoftware
+	make cleandb
+	make install
+	make webservice
+	make annotation
+	make genome
 else
     echo "Not in zippy folder."
     sudo mkdir -p ${srv_zippy}
@@ -16,12 +22,5 @@ else
     git clone --recursive https://github.com/Lucioric2000/zippy
     sudo chmod -R 777 ${srv_zippy}
     cd zippy
-    
+    ./zippy_install.bash $@
 fi
-
-make cleansoftware
-make cleandb
-make install
-make webservice
-make annotation
-make genome
