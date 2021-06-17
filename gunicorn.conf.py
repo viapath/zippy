e613838e95315fcbc7ca099a7dc23aa9e8b34f85
David Brawand <david.brawand@gmail.com>
2021-06-17 11:03:17 +0100

    increased gunicorn log level and enabled reloading

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -7,10 +7,10 @@ import multiprocessing
 _LOGS = '/var/local/zippy'
 errorlog = os.path.join(_LOGS, 'zippy_error.log')
 accesslog = os.path.join(_LOGS, 'zippy_access.log')
-loglevel = 'info'
+loglevel = 'debug'
+reload = True
 bind = '0.0.0.0:5000'
 workers = multiprocessing.cpu_count() * 2 + 1
 timeout = 3 * 60  # timeout 3 minutes
 keepalive = 24 * 60 * 60  # keep connections alive for 1 day
 capture_output = True
-
